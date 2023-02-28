7a0f2e94e3968f74ea5a36beb1cede24ec6560af
Abhinav Gupta <mail@abhinavg.net>
2023-02-27 19:52:03 -0800

    test: Switch to YAML testdata (#26)
    Switches to a custom YAML-based testdata format
    instead of goldmark's test utilities.
    Also runs them all in parallel.
    
    Similar to abhinav/goldmark-hashtag#20

--- integration_test.go
+++ integration_test.go
@@ -2,23 +2,42 @@ package wikilink_test
 
 import (
 	"bytes"
+	"os"
 	"testing"
 
+	"github.com/stretchr/testify/require"
 	"github.com/yuin/goldmark"
-	goldtestutil "github.com/yuin/goldmark/testutil"
 	"go.abhg.dev/goldmark/wikilink"
+	"gopkg.in/yaml.v3"
 )
 
 func TestIntegration(t *testing.T) {
 	t.Parallel()
 
-	goldtestutil.DoTestCaseFile(
-		goldmark.New(goldmark.WithExtensions(&wikilink.Extender{
-			Resolver: _resolver,
-		})),
-		"testdata/tests.txt",
-		t,
-	)
+	testsdata, err := os.ReadFile("testdata/tests.yaml")
+	require.NoError(t, err)
+
+	var tests []struct {
+		Desc string `yaml:"desc"`
+		Give string `yaml:"give"`
+		Want string `yaml:"want"`
+	}
+	require.NoError(t, yaml.Unmarshal(testsdata, &tests))
+
+	md := goldmark.New(goldmark.WithExtensions(&wikilink.Extender{
+		Resolver: _resolver,
+	}))
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.Desc, func(t *testing.T) {
+			t.Parallel()
+
+			var buf bytes.Buffer
+			require.NoError(t, md.Convert([]byte(tt.Give), &buf))
+			require.Equal(t, tt.Want, buf.String())
+		})
+	}
 }
 
 var (
